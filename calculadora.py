4e8eb4fabccc18f7c5af7833eef228fb8f53924e
Allison p p santos <135356286+AllisonPaes@users.noreply.github.com>
2023-10-26 15:12:02 -0300

    Calculadora.py
    Este é um exemplo básico de uma calculadora. Ela permite ao usuário selecionar a operação desejada e, em seguida, inserir os números para realizar a operação. Certifique-se de que os valores de entrada sejam tratados adequadamente para evitar erros, como a divisão por zero.

---
+++ calculadora.py
@@ -0,0 +1,43 @@
+def adicao(x, y):
+    return x + y
+
+def subtracao(x, y):
+    return x - y
+
+def multiplicacao(x, y):
+    return x * y
+
+def divisao(x, y):
+    if y == 0:
+        return "Erro: Divisão por zero"
+    return x / y
+
+while True:
+    print("Escolha a operação:")
+    print("1. Adição")
+    print("2. Subtração")
+    print("3. Multiplicação")
+    print("4. Divisão")
+    print("5. Sair")
+
+    escolha = input("Digite a opção (1/2/3/4/5): ")
+
+    if escolha == '5':
+        print("Calculadora encerrada.")
+        break
+
+    if escolha not in ('1', '2', '3', '4'):
+        print("Opção inválida. Por favor, escolha uma opção válida.")
+        continue
+
+    num1 = float(input("Digite o primeiro número: "))
+    num2 = float(input("Digite o segundo número: "))
+
+    if escolha == '1':
+        print("Resultado:", adicao(num1, num2))
+    elif escolha == '2':
+        print("Resultado:", subtracao(num1, num2))
+    elif escolha == '3':
+        print("Resultado:", multiplicacao(num1, num2))
+    elif escolha == '4':
+        print("Resultado:", divisao(num1, num2))
